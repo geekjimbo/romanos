9049cd3dd2520642426533135bd79323e1c52646
geekjimbo <jimmyfigueroa@me.com>
2016-02-03 14:39:34 -0600

    version1 hasta el 9

--- romanos_rree.rb
+++ romanos_rree.rb
@@ -1,4 +1,16 @@
 def convert(in_arabic)
   return "" if in_arabic == 0 
-  "IV"
+  arabigo, romano = convert_factor(in_arabic)
+  romano + convert(in_arabic - arabigo)
+end
+
+ARABIGOS = [
+            [9, "IX"],
+            [5, "V"],
+            [4, "IV"],
+            [1, "I"]
+           ]
+
+def convert_factor(in_arabic)
+  ARABIGOS.find{|arabigo, _| arabigo <= in_arabic}
 end
--- tdd_romanos_rree.rb
+++ tdd_romanos_rree.rb
@@ -2,10 +2,14 @@ require './romanos_rree.rb'
 
 describe "convertir de arabigos a romanos" do 
 
-  { 0 => "",
-    4 => "IV",
+  { 
+    9 => "IX",
+    6 => 'VI',
     5 => "V",
-    9 => "IX"
+    4 => "IV",
+    2 => 'II',
+    1 => 'I',
+    0 => ""
   }.each do |arabigo, romano|
     it "ingresar #{arabigo} y recibir un #{romano}" do
       expect(convert(arabigo)).to eq(romano)
